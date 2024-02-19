6f3fbb1caaf8d7f7e9f982a7d8aa38a94baf577a
Amir Poudel <amirpoudel2058@gmail.com>
2024-02-19 22:34:35 +0545

    set direct connect to redis for docker

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -3,10 +3,12 @@ import { createClient, RedisClientOptions } from "redis";
 interface CustomRedisClientOptions extends RedisClientOptions {
     host: string;
 }
+console.log("redis host", process.env.REDIS_HOST);
+console.log("redis port", process.env.REDIS_PORT);
 
 export const redisClient = createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    host:'ubuntu-redis-1',
+    port:6379,
     
 } as CustomRedisClientOptions).on("error", (error) => {
     console.log("error in redis client", error);
