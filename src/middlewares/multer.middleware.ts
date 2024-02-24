c02f26e5369a5687c76bb7a86b1489b174b21a6c
Amir Poudel <amirpoudel2058@gmail.com>
2024-02-24 13:26:38 +0545

    add image type and size in multer

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -23,6 +23,21 @@ const ensureDestinationDirectory = (destination:string) => {
     }
 };
 
+
+const fileFilter = function (req:any, file:any, cb:Function) {
+    // Accept only files with mime types image/jpeg, image/png, or image/gif
+    if (
+        file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/gif'||
+        file.mimetype === 'image/jpg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only JPEG, PNG, or GIF files are allowed!'), false);
+    }
+};
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         const destinationPath = "./public/temp/images"; // Relative path for demonstration
@@ -30,8 +45,14 @@ const storage = multer.diskStorage({
         cb(null, destinationPath);
     },
     filename: generateUniqueFileName,
+
+
 });
 
 export const upload = multer({
     storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 1, // 1MB
+    },
 });
